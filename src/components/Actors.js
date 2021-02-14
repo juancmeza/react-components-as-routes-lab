d0cd9e19caddc8df6d4c360f34d6d8d5dcff1a1f
juan_carlos_meza <juancmezat@gmail.com>
2021-02-13 23:58:03 -0800

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -6,7 +6,7 @@ const Actors = () => {
     <div>
       <h1>Actors Page</h1>
         {actors.map(actor => <div>
-     <h2>Name: {actor.name}</h2>
+         <h2>Name: {actor.name}</h2>
            <ul>{actor.movies.map(movie => <li>{movie}</li>)}</ul> 
           </div>)}
     </div>
